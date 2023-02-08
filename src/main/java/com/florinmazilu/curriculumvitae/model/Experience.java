1d2990055bb6005b349bba36965e007c0266b692
Florin <maziluflorin84@outlook.com>
2023-02-09 01:18:08 +0200

    JPA

---
+++ src/main/java/com/florinmazilu/curriculumvitae/model/Experience.java
@@ -0,0 +1,100 @@
+package com.florinmazilu.curriculumvitae.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class Experience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String company;
+    private Date start;
+    private Date end;
+    private boolean isPresent;
+    private String description;
+    private Set<Skill> skills;
+
+    public Experience() {
+    }
+
+    public Experience(String title, String company, Date start, Date end, boolean isPresent, String description, Set<Skill> skills) {
+        this.title = title;
+        this.company = company;
+        this.start = start;
+        this.end = end;
+        this.isPresent = isPresent;
+        this.description = description;
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public boolean isPresent() {
+        return isPresent;
+    }
+
+    public void setPresent(boolean present) {
+        isPresent = present;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<Skill> skills) {
+        this.skills = skills;
+    }
+}
---
+++ src/main/java/com/florinmazilu/curriculumvitae/model/Skill.java
@@ -0,0 +1,37 @@
+package com.florinmazilu.curriculumvitae.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+    public Skill() {
+    }
+
+    public Skill(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
