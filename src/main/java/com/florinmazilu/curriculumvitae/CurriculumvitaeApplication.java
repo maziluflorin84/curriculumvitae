b33aa0804bddfa593e6674917a2cd864ac696182
Florin <maziluflorin84@outlook.com>
2023-02-08 23:49:40 +0200

    Initial commit

---
+++ src/main/java/com/florinmazilu/curriculumvitae/CurriculumvitaeApplication.java
@@ -0,0 +1,13 @@
+package com.florinmazilu.curriculumvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumvitaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumvitaeApplication.class, args);
+	}
+
+}
